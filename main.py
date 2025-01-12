bd2f9813642715953cf6e385f9775b14c608159e
noapatch <noapatch@post.bgu.ac.il>
2025-01-12 16:49:59 +0200

    adding notes in the main to make it more readable for others

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import threading
 from Configuration import Configuration
 from Client.Client import Client
 from Server.Server import Server
-
+import time
 
 def start_server():
+    # Starting the server side, using configuration file to easily change what we need
     config = Configuration().get_config()
     server_ip = config.server_ip
     tcp_port = config.tcp_port
     udp_port = config.udp_port
-
+    #creat the server object and start it
     server = Server(server_ip, tcp_port, udp_port)
     server.start()
 
 
 def start_client():
+    # creating a Client object and starting it
     client = Client()
     client.start()
 
@@ -24,8 +26,7 @@ def main():
     server_thread = threading.Thread(target=start_server)
     server_thread.daemon = True
     server_thread.start()
-
-    import time
+    # set the server thread to sleep to creat the client
     time.sleep(1)
 
     start_client()
